5a2ca1f8c1cfbc0f62966f998a6f89d95f9395e1
gitexpertneo <gitexpert.neo@gmail.com>
2022-05-28 18:45:15 +0530

    Added new file TestGit2 and modified TestGit

--- TestGit.java
+++ TestGit.java
@@ -3,5 +3,7 @@ class TestGit
 	public static void main(String args[])
 	{
 		System.out.println("Halo Test Git project!!!");
+		System.out.println("Welcome to Java Git Master Training!!");
+		System.out.println("neonetworks.in!!");
 	}
 }
---
+++ TestGit2.java
@@ -0,0 +1,7 @@
+class TestGit2
+{
+	public static void main(String args[])
+	{
+		System.out.println("Halo from Second TestGit2");
+	}
+}
